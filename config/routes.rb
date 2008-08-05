c97690c28967e0f91ce7b3e2bdf9397ffc171ae4
David Beckwith <dbitsolutions@gmail.com>
2008-08-05 04:57:05 -0700

    Added user authentication with RSpec tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  
+  map.resources :users
+  map.resource :session
   map.resources :time_entries
+  
   map.root :controller => "time_entries"
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080805081753) do
+ActiveRecord::Schema.define(:version => 20080805115341) do
 
   create_table "time_entries", :force => true do |t|
     t.datetime "start_time"
@@ -17,5 +17,19 @@ ActiveRecord::Schema.define(:version => 20080805081753) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",                     :limit => 40
+    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "email",                     :limit => 100
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token",            :limit => 40
+    t.datetime "remember_token_expires_at"
+  end
+
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
